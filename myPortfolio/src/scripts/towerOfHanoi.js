13cb77a14da2f2c90d439c5be1b0e02b79e8899d
laavang <laavang@gmail.com>
2019-12-15 17:49:05 -0600

    Fixed ring and column double action

--- myPortfolio/src/scripts/towerOfHanoi.js
+++ myPortfolio/src/scripts/towerOfHanoi.js
@@ -8,17 +8,28 @@ $(document).ready(function() {
     var selectedRing = null;
     var selectedColumn = null;
 
-
-        $('.ring').click(function() {
-            selectedRing = $(this);
-            console.log(selectedRing.attr('id') + ' ' + 'has been selected');
-            $(selectedRing).detach();
+    
+        $('.ring').click(function(event) {
+            if (selectedRing == null) {
+                selectedRing = $(this);
+                console.log(selectedRing.attr('id') + ' ' + 'has been selected');
+                $(selectedRing).detach();
+                event.stopPropagation();
+            }
+            else {
+            }
      });
 
-        $('.column').click(function() {
-            selectedColumn = $(this);
-            console.log(selectedColumn.attr('id') + ' ' + 'has been selected');
-            $(selectedColumn).prepend(selectedRing);
+        $('.column').click(function(event) {
+            if (selectedRing != null) {
+                selectedColumn = $(this);
+                console.log(selectedColumn.attr('id') + ' ' + 'has been selected');
+                $(selectedColumn).prepend(selectedRing);
+                selectedRing = null;
+                event.stopPropagation();
+            }
+            else {
+            }
         });
 
 
